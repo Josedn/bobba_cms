a90331b3b9cd3135d55f3ed17ee21a32232f76be
Jose D <joserafael161@hotmail.com>
2019-05-19 23:49:17 -0500

    Handle index form

--- src/components/IndexPage/IndexHeader.js
+++ src/components/IndexPage/IndexHeader.js
@@ -1,24 +1,93 @@
 import React from 'react';
 import Logo from '../Generic/Logo';
 
+const initialState = {
+    username: '',
+    password: '',
+    rememberme: false,
+    errorMessage: '',
+    wrongPassword: false,
+    wrongUsername: false,
+};
+
 class IndexHeader extends React.Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = initialState;
+    }
+
+    handleInputChange = event => {
+        const target = event.target;
+        const value = target.type === 'checkbox' ? target.checked : target.value;
+        const name = target.name;
+        this.setState({
+            [name]: value
+        });
+        console.log(this.state);
+    }
+
+    handleSubmit = event => {
+        event.preventDefault();
+
+        const wrongUsername = this.state.username === '';
+        const wrongPassword = this.state.password === '';
+        let errorMessage = '';
+
+        if (wrongUsername) {
+            errorMessage = 'Por favor, ingresa tu nombre de usuario y contraseña';
+        } else if (wrongPassword) {
+            errorMessage = 'Por favor, ingresa tu contraseña';
+        }
+        if (errorMessage === '') {
+            this.setState(initialState);
+        } else {
+            this.setState({
+                wrongUsername, wrongPassword, errorMessage
+            });
+        }
+    }
+
+    getErrorSection(errorMessage) {
+        return (
+            <section className="login_error">
+                {errorMessage}
+            </section>
+        );
+    }
+
     render() {
+
+        const { username, password, rememberme, errorMessage, wrongUsername, wrongPassword } = this.state;
+
+        let errorContainer = null;
+        if (errorMessage !== '') {
+            errorContainer = this.getErrorSection(errorMessage);
+        }
+
+        let usernameClassName = wrongUsername ? 'wrong' : '';
+        let passwordClassName = wrongPassword ? 'wrong' : '';
+
         return (
             <header>
+                {errorContainer}
                 <div className="header_container">
-                    <Logo/>
-                    <form>
+                    <Logo />
+                    <form onSubmit={this.handleSubmit}>
                         <div className="input_group">
-                            <label htmlFor="user">Usuario: </label>
-                            <input id="user" name="user" type="text" aria-label="Usuario" placeholder="Usuario" />
+                            <label htmlFor="username">Usuario: </label>
+                            <input id="username" name="username" type="text" aria-label="Usuario" placeholder="Usuario"
+                                onChange={this.handleInputChange} value={username} className={usernameClassName} />
                         </div>
                         <div className="input_group">
                             <label htmlFor="password">Contraseña: </label>
-                            <input className="wrong" id="password" name="password" type="password" aria-label="Contraseña"
-                                placeholder="Contraseña" />
+                            <input id="password" name="password" type="password" aria-label="Contraseña"
+                                placeholder="Contraseña" onChange={this.handleInputChange} value={password}
+                                className={passwordClassName} />
                         </div>
                         <div className="input_group">
-                            <input id="rememberme" name="rememberme" type="checkbox" />
+                            <input id="rememberme" name="rememberme" type="checkbox" onChange={this.handleInputChange} checked={rememberme} />
                             <label htmlFor="rememberme">Recuérdame</label>
                             <input type="submit" value="Entrar" aria-label="Entrar" />
                         </div>
