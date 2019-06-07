e9a1783e2d2ce009b7c7494f0a657546a2820037
Jose D <joserafael161@hotmail.com>
2019-06-06 23:10:20 -0500

    Fix userdata fetch on client

--- src/components/Pages/ClientPage/ClientPage.js
+++ src/components/Pages/ClientPage/ClientPage.js
@@ -2,24 +2,43 @@ import React from 'react';
 import { connect } from 'react-redux';
 import LoggedRedirectorContainer from '../../../containers/LoggedRedirectorContainer';
 import { getClientUrl } from '../../../controllers/BobbaProxy';
+import { logOut, userSetData } from '../../../actions';
+import { tryGetUserData } from '../../../controllers/BobbaProxy';
 
 class ClientPage extends React.Component {
+    componentDidMount() {
+        const { userContext, loginContext, dispatch } = this.props;
+        if (!userContext.fetched) {
+            tryGetUserData(loginContext.token).then(response => {
+                if (response.error != null) {
+                    dispatch(logOut());
+                } else {
+                    dispatch(userSetData(response.username, response.motto, response.look));
+                }
+            });
+        }
+    }
 
     render() {
-        const { username, look } = this.props.loginContext;
-        const src = getClientUrl(username, look);
+        const { username, look, fetched } = this.props.userContext;
 
+        if (!fetched) {
+            return (
+                <div className="client" />
+            );
+        }
+
+        const src = getClientUrl(username, look);
         return (
             <div className="client">
-              <LoggedRedirectorContainer />
-              <iframe title='Bobba' src={src}>
-
-              </iframe>
+                <LoggedRedirectorContainer />
+                <iframe title='Bobba' src={src} />
             </div>
         );
     }
 }
 const mapStateToProps = state => ({
+    userContext: state.user,
     loginContext: state.login,
 });
 
